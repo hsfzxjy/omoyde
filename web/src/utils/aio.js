8bf6829b2b3ae3ce0d2fee9943abef38e96160cd
hsfzxjy <hsfzxjy@gmail.com>
2022-02-16 14:33:18 +0800

    add:web: `Deferred`

--- web/src/utils/aio.js
+++ web/src/utils/aio.js
@@ -4,3 +4,28 @@ export const sleep = (milliseconds) =>
   })
 
 export const setImmediate = (cb) => setTimeout(cb, 0)
+
+export class Deferred {
+  constructor() {
+    this._resolve = null
+    this._promise = null
+  }
+  pending() {
+    return this._promise !== null
+  }
+  wait() {
+    if (this._promise === null) return
+    return this._promise
+  }
+  reset() {
+    if (this._promise !== null)
+      throw new Error("cannot reset a pending Deferred")
+    this._promise = new Promise((resolve) => (this._resolve = resolve))
+  }
+  resolve() {
+    if (this._promise === null)
+      throw new Error("cannot resolve a resolved Deferred")
+    this._resolve()
+    this._promise = this._resolve = null
+  }
+}
