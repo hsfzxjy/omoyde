ca20ed290a14afde629c33832914a11c75088814
hsfzxjy <hsfzxjy@gmail.com>
2022-02-16 13:43:07 +0800

    add:web: hot keys for convenient login/unlock during development

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import { loginWithPassword, unlockWithPincode } from "./infrastructures/auth"
 
 createApp(App).mount("#app")
+
+if (import.meta.env.MODE === "development") {
+  window.addEventListener("keydown", (evt) => {
+    if (!evt.altKey) return
+    switch (evt.key) {
+      case "U":
+        unlockWithPincode(import.meta.env.VITE_SECURITY_PINCODE)
+        break
+      case "L":
+        loginWithPassword(import.meta.env.VITE_SECURITY_PASSWORD)
+        break
+    }
+  })
+}
