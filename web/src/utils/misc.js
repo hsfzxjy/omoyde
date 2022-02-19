5f0299d6ed987c7359619d75e6d8b00e1bbe419d
hsfzxjy <hsfzxjy@gmail.com>
2022-02-19 15:16:28 +0800

    add:web: `dispatch()`

--- web/src/utils/misc.js
+++ web/src/utils/misc.js
@@ -62,5 +62,26 @@ export function debounce(cb, timeout, setupCtx) {
   return handler
 }
 
+export function dispatch(routes, thisArg) {
+  const keys = Object.keys(routes)
+  return function (options) {
+    let that
+    switch (typeof thisArg) {
+      case "function":
+        that = thisArg()
+        break
+      case "undefined":
+        that = this
+        break
+      default:
+        that = thisArg
+    }
+    for (const [key, method] of Object.entries(routes)) {
+      if (options[key] !== undefined) return that[method](options)
+    }
+    throw new Error(`should specify one of ${keys}`)
+  }
+}
+
 export const patch = Object.assign
 export const noop = () => {}
