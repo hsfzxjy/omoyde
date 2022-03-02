51f931ef4b10ae3a644130158c17165f9387dd20
hsfzxjy <hsfzxjy@gmail.com>
2022-03-02 13:04:04 +0800

    add:web: src/components/BasicFlowItemToolbar.vue

---
+++ web/src/components/BasicFlowItemToolbar.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { computed, inject, toRaw } from "vue"
+import { store } from "../states"
+import { clone } from "../utils/misc"
+
+const props = defineProps({
+  data: Object,
+  globalIndex: Number,
+  localIndex: Number,
+})
+const dataSource = inject("dataSource")
+const flowBus = inject("flowBus")
+const show = computed(() => store.fragment.editting)
+const edittable = computed(() => props.data.kind === "msg")
+
+function detachedItem() {
+  const item = clone(toRaw(props.data))
+  delete item.prevDt
+  delete item.dt
+  return item
+}
+
+function onMoveUp() {
+  const index = props.globalIndex
+  if (index === 0) return
+  dataSource.moveForward(index, detachedItem())
+  flowBus.emit("update-index", index - 1, props.localIndex)
+}
+function onMoveDown() {
+  const index = props.globalIndex
+  if (index === dataSource.countAll() - 1) return
+  dataSource.moveBackward(index, detachedItem())
+  flowBus.emit("update-index", index + 1, props.localIndex)
+}
+</script>
+
+<template>
+  <template v-if="show">
+    <div class="basic-flow-item-toolbar top">
+      <div class="basic-flow-item-toolbar-button">ADD</div>
+      <template v-if="edittable">
+        <div class="basic-flow-item-toolbar-button">EDIT</div>
+        <div class="basic-flow-item-toolbar-button">DEL</div>
+        <div class="basic-flow-item-toolbar-splitter"></div>
+        <div class="basic-flow-item-toolbar-button" @click="onMoveDown">
+          DOWN
+        </div>
+        <div class="basic-flow-item-toolbar-button" @click="onMoveUp">UP</div>
+      </template>
+    </div>
+    <div class="basic-flow-item-toolbar bottom">
+      <div class="basic-flow-item-toolbar-button">ADD</div>
+    </div>
+    <div class="basic-flow-item-toolbar spacing"></div>
+    <div class="basic-flow-item-brace"></div>
+  </template>
+</template>
+
+<style lang="scss">
+.basic-flow-item-brace {
+  position: absolute;
+  width: 5vw;
+  top: 0;
+  left: 5vw;
+  bottom: 0;
+  border-width: 2px 0 2px 2px;
+  border-style: dashed;
+  border-color: #777;
+}
+
+.basic-flow-item-toolbar {
+  display: flex;
+  justify-content: space-around;
+  width: 80vw;
+
+  &.top {
+    border-top: #777 dashed 1px;
+    order: -2;
+  }
+
+  &.bottom {
+    border-bottom: #777 dashed 1px;
+    order: 10;
+  }
+
+  &.spacing {
+    height: 0.8rem;
+    order: 11;
+
+    + .basic-flow-item-brace {
+      bottom: 0.8rem;
+    }
+  }
+}
+.basic-flow-item-toolbar-splitter {
+  flex-grow: 0.6;
+}
+
+.basic-flow-item-toolbar-button {
+  font-size: 0.75rem;
+  padding: 0.5rem;
+  text-align: center;
+  background-color: #ddd;
+}
+</style>
