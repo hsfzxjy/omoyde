a2f7e074bec5c583d6633359cfe156295ab9c5c0
hsfzxjy <hsfzxjy@gmail.com>
2022-03-02 13:02:50 +0800

    change:web: reload current flow after editting state changed

--- web/src/components/TheMainContainer.vue
+++ web/src/components/TheMainContainer.vue
@@ -1,12 +1,34 @@
 <script setup>
 import BasicFlow from "./BasicFlow.vue"
+import TransitionFlow from "./TransitionFlow.vue"
+import { store, bus } from "../states"
+import { computed, nextTick } from "vue"
+import { Mutex } from "../utils/aio"
+
+const currentFlow = computed(() => {
+  return {
+    BasicFlow,
+    TransitionFlow,
+  }[store.ui.flow]
+})
+
+const flowMutex = new Mutex()
+bus.on("edittingChanged", () =>
+  flowMutex.guardOrSkip(async () => {
+    const oldFlow = store.ui.flow
+    store.ui.flow = "TransitionFlow"
+    await nextTick()
+    store.ui.flow = oldFlow
+    await nextTick()
+  })
+)
 </script>
 
 <template>
   <div id="main">
-    <Suspense>
-      <basic-flow />
-    </Suspense>
+    <suspense>
+      <component :is="currentFlow" />
+    </suspense>
   </div>
 </template>
 
