416744f6ef857ae46a7caf2d81153cc617e08aaf
hsfzxjy <hsfzxjy@gmail.com>
2022-03-02 21:37:47 +0800

    change:web: improve newDt strategy

--- web/src/services/media/misc.js
+++ web/src/services/media/misc.js
@@ -6,6 +6,17 @@ export function timeGapIsLarge(dt, prevDt) {
   return dt - prevDt >= 1000 * 60 * 30 // 30 minutes
 }
 
+function between(x, y) {
+  const MAX_STEP = 0.1
+  const sign = x > y ? 1 : -1
+  const gap = sign === 1 ? x - y : y - x
+  if (gap >= 2 * MAX_STEP) {
+    return x - sign * MAX_STEP
+  } else {
+    return (x + y) / 2
+  }
+}
+
 export function moveForward(dataSource, index, item) {
   item = clone(toRaw(item))
   const {
@@ -22,7 +33,8 @@ export function moveForward(dataSource, index, item) {
     const newDt =
       prev1kind === ITEM_NULL || timeGapIsLarge(prev0dt, prev1dt)
         ? prev0dt - 1
-        : (prev0dt + prev1dt) / 2
+        : between(prev0dt, prev1dt)
+
     item.dt = newDt
     dataSource.moveForward(index, item)
     return index - 1
@@ -45,7 +57,8 @@ export function moveBackward(dataSource, index, item) {
     const newDt =
       next1kind === ITEM_NULL || timeGapIsLarge(next1dt, next0dt)
         ? next0dt + 1
-        : (next0dt + next1dt) / 2
+        : between(next0dt, next1dt)
+
     item.dt = newDt
     dataSource.moveBackward(index, item)
     return index + 1
