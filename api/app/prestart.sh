64c7c3b25ab6b769aaab30416a057812aaf70025
hsfzxjy <hsfzxjy@gmail.com>
2022-03-07 21:28:52 +0800

    change:api: watch changes of .so files in dev mode

--- api/app/prestart.sh
+++ api/app/prestart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 service redis-server start
+
+if test "$0" = "/start-reload.sh"; then
+    exec uvicorn --reload --reload-include="*.so" --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
+fi
